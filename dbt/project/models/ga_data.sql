6b28dd62bdb314467d1c7b5d07b5866379a771b6
Julian Dütsch <julian.duetsch@btelligent.com>
2022-09-02 11:40:04 +0200

    fixed sql

--- dbt/project/models/ga_data.sql
+++ dbt/project/models/ga_data.sql
@@ -1,3 +1,3 @@
 SELECT SUM(totals.pageviews) AS TotalPageviews
 
-FROM 'bigquery-public-data.google_analytics_sample.ga_sessions_20170101'
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170101`
